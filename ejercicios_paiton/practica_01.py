3adeffdca3cb41aca66d468b74a89b9e5ebb485c
NaylaVg <naylamaitevega@gmail.com>
2025-10-06 09:19:23 -0300

    agregando ejercicios

---
+++ ejercicios_paiton/practica_01.py
@@ -0,0 +1,16 @@
+#Crea una lista vacía llamada lista_compras
+# Luego:
+#1- Agregá 3 productos usando .append() 
+#2- Mostrá cuántos productos hay con len()
+#3- Eliminá el último producto con .pop()
+#4- Mostrá la lista actualizada
+#El objetivo es aprender .append(), .pop() y .len()
+
+lista_compras = []
+
+lista_compras.append("frutilla")
+lista_compras.append("chocolate")
+lista_compras.append("arroz")
+
+print("Esta es tu lista de compras: :", len(lista_compras))
+
---
+++ ejercicios_paiton/practica_02.py
@@ -0,0 +1,14 @@
+#colores = ["rojo", "azul", "verde", "rojo", "azul", "rojo"]
+#1- Mostrá cuántas veces aparece “rojo” usando .count(). 
+#2- Reemplazá el primer “verde” por “amarillo”
+#3- Mostrá la lista final
+
+colores = ["rojo", "azul", "verde", "rojo", "azul", "rojo"]
+
+cantidad_rojo = colores.count("rojo")
+print("cantidad_rojo")
+
+indice_verde = colores.index("verde")
+print("indice_verde")
+
+
