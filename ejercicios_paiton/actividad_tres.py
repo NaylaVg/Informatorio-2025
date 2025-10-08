a1bf47b347d5af33952e2346947476e7cd7b62bf
NaylaVg <naylamaitevega@gmail.com>
2025-10-08 08:30:47 -0300

    Guardando cambios

--- ejercicios_paiton/actividad_tres.py
+++ ejercicios_paiton/actividad_tres.py
@@ -6,3 +6,20 @@
 # C- Si existe, muestre por pantalla el precio y la cantidad disponible
 # D- Si no existe, deja un mensaje de que no se encontró el producto
 # }
+
+stock = {}
+
+
+for i in range(4):
+    nombre = input("Ingresa el nombre del producto: ")
+    precio = input(f"Ingresa el precio del {nombre}: ")
+    cantidad_disponible = input(f"Ingresa la cantidad disponible de {nombre}: ")
+    stock[nombre] = [precio, cantidad_disponible]
+
+buscar_producto = input(f"Ingresa el producto que solicita buscar: ")
+
+if buscar_producto in stock:
+    stock[buscar_producto] = [precio, cantidad_disponible]
+    print(f"¡Producto encontrado!, Nombre: {buscar_producto}, Precio: ${precio}, cantidad: {cantidad_disponible}")
+else:
+    print(f"No se encontró el producto indicado")
--- ejercicios_paiton/actividad_uno.py
+++ ejercicios_paiton/actividad_uno.py
@@ -15,3 +15,19 @@
 
 # La cantidad de números impares
 
+numeros = []
+
+for i in range(10):
+    n = int(input(f"Ingresa un numero: "))
+    numeros.append(n)
+
+pares = [n for n in numeros if n %2 ==0]
+impares = [n for n in numeros if n %2 !=0]
+
+suma_pares = sum(pares)
+print(f"La suma de todos los números pares es: ", suma_pares)
+
+cantidad_impares = len(impares)
+print(f"La cantidad de números pares ingresados son: ", cantidad_impares)
+
+print(f"La lista completa de los números es: ", numeros)
